72f70530816f0b4b6a4a2a89dc68790d10e3d90e
Srinivas <ex63@global.tesco.org>
2019-11-24 09:59:07 +0530

    docker image add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  hostV1: 'https://node-url-shorten.herokuapp.com',
+  // hostV1: 'https://node-url-shorten.herokuapp.com',
+  hostV1: 'http://localhost:4000',
 };
